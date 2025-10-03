db6f8b426dd357f182da98dd55048930f70241f5
ssuparare <114672040+ssuparare@users.noreply.github.com>
2025-10-03 20:19:04 +0700

    Add files via upload

---
+++ search_mul.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import multiprocessing
+
+#เก็บไฟล์ text ที่ได้มาไว้ที่ไหนเอามาใส่
+path = Path('dir')
+
+#เลข นร.
+target = 'student id'
+
+def searching(search_string):
+    for file in path.rglob('*.bin'):
+        if file.is_file():
+            text = file.read_text()
+        if search_string in text:
+            print("found in student id in: ")
+            print(file)
+
+if __name__ == "__main__":
+    proccess1 = multiprocessing.Process(target=searching, args=(target, ))
+
+    proccess1.start()
+    proccess1.join()
+
+    print("done(mult)")
---
+++ search_seq.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+#####   เก็บไฟล์ text ที่ได้มาไว้ที่ไหนเอามาใส่  #####
+#####   c:/user/file    #####
+#####   ใช้ / ไม่ใช่ \     #####
+path = Path('dir')
+
+#####   เลข นศ.
+search_string = 'student id'
+
+for file in path.rglob('*.bin'):
+    if file.is_file():
+        text = file.read_text()
+        if search_string in text:
+            print("found in student id in: ")
+            print(file)
+
+print("done(seq)")
